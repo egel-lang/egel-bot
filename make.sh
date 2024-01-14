76e8d93324074b8b5ba41819198afc7b2471c6d4
Marco Devillers <marco.devillers@gmail.com>
2024-01-14 19:14:31 +0100

    shortcut

---
+++ make.sh
@@ -0,0 +1,1 @@
+clang++ ../src/egel-bot.cpp -o egel-bot -std=c++20 -I../../egel/vendor/local/include -I../../egel/src -L../../egel/vendor/local/lib -llightning -licudata -licui18n -licuio -licuuc -licutu -lfmt
